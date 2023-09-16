3ab94e4a2886b5565a64b8b01d347272c226b578
Miika Savela <miika.a.savela@gmail.com>
2023-09-16 14:00:38 +0300

    Adjust age category names and filter layout

--- src/routes/leikki/+page.svelte
+++ src/routes/leikki/+page.svelte
@@ -15,7 +15,7 @@
 	let selectedGameType: GameType = data.selectedGameType || gameTypes[0];
 
 	let ageCategories: AgeCategory[] = [
-		{ id: 0, name: 'Kaikki', minAge: 0, maxAge: 12 },
+		{ id: 0, name: 'Kaikenikäiset', minAge: 0, maxAge: 12 },
 		...data.ageCategories
 	];
 	let selectedAgeCategory: AgeCategory = data.selectedAgeCategory || ageCategories[0];
@@ -50,56 +50,58 @@
 </script>
 
 <section class="mb-8">
-	<h2 class="text-5xl font-heading mb-8">Leikit</h2>
+	<h2 class="text-5xl font-heading mb-4">Leikit</h2>
 	<div class="mb-16">
-		<h3 class="mb-2">Ikä</h3>
-		<fieldset class="flex">
-			{#each ageCategories as ageCategory}
-				<label
-					class="capitalize p-4 mr-2 bg-slate-100 focus-within:bg-slate-200 text-sm flex flex-col justify-center items-center"
-					class:bg-slate-400={selectedAgeCategory.name === ageCategory.name}
-					for={ageCategory.name}
-				>
-					<p>{ageCategory.minAge}-{ageCategory.maxAge}</p>
-					<p>
-						{ageCategory.name}
-					</p>
-					<input
-						type="radio"
-						class="sr-only"
-						name={ageCategory.name}
-						id={ageCategory.name}
-						value={ageCategory}
-						bind:group={selectedAgeCategory}
-						on:change={() => updateSearchParams('ageCategory', ageCategory.id?.toString() ?? '0')}
-					/>
-				</label>
-			{/each}
-		</fieldset>
-		<h3 class="mb-2">Leikkitavat</h3>
-		<fieldset class="flex">
-			{#each gameTypes as gameType}
-				<label
-					class="capitalize w-20 h-20 mr-2 bg-slate-100 focus-within:bg-slate-200 text-sm flex flex-col justify-center items-center"
-					class:bg-slate-400={selectedGameType.name === gameType.name}
-					for={gameType.id?.toString()}
-				>
-					<Icon name={gameType.iconName} />
-					<span>
-						{gameType.name}
-					</span>
-					<input
-						type="radio"
-						class="sr-only"
-						name={gameType.name}
-						id={gameType.id?.toString()}
-						value={gameType}
-						bind:group={selectedGameType}
-						on:change={() => updateSearchParams('gameType', gameType.id?.toString() ?? '0')}
-					/>
-				</label>
-			{/each}
-		</fieldset>
+		<div id="game-type-filter" class="mb-8">
+			<fieldset class="flex gap-2">
+				{#each gameTypes as gameType}
+					<label
+						class="capitalize w-20 h-20 bg-slate-100 focus-within:bg-slate-200 text-sm flex flex-col justify-center items-center"
+						class:bg-slate-400={selectedGameType.name === gameType.name}
+						for={gameType.id?.toString()}
+					>
+						<Icon name={gameType.iconName} />
+						<span>
+							{gameType.name}
+						</span>
+						<input
+							type="radio"
+							class="sr-only"
+							name={gameType.name}
+							id={gameType.id?.toString()}
+							value={gameType}
+							bind:group={selectedGameType}
+							on:change={() => updateSearchParams('gameType', gameType.id?.toString() ?? '0')}
+						/>
+					</label>
+				{/each}
+			</fieldset>
+		</div>
+		<div id="age-category-filter">
+			<fieldset class="flex flex-wrap gap-2">
+				{#each ageCategories as ageCategory}
+					<label
+						class="text-center capitalize p-4 bg-slate-100 focus-within:bg-slate-200 text-sm flex flex-col justify-center items-center w-[90px]"
+						class:bg-slate-400={selectedAgeCategory.name === ageCategory.name}
+						for={ageCategory.name}
+					>
+						<p class="text-lg mb-2">{ageCategory.minAge}-{ageCategory.maxAge}</p>
+						<p class="text-xs">
+							{ageCategory.name}
+						</p>
+						<input
+							type="radio"
+							class="sr-only"
+							name={ageCategory.name}
+							id={ageCategory.name}
+							value={ageCategory}
+							bind:group={selectedAgeCategory}
+							on:change={() => updateSearchParams('ageCategory', ageCategory.id?.toString() ?? '0')}
+						/>
+					</label>
+				{/each}
+			</fieldset>
+		</div>
 	</div>
 	<div class="space-y-4">
 		{#if data.games.length > 0}
